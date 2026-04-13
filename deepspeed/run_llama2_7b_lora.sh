3fe9790a6f1146186d98e5b252cf3a731fdc62b2
Jackshen-02 <yuntian2@andrew.cmu.edu>
2026-04-13 00:06:34 -0400

    fix bash file

--- deepspeed/run_llama2_7b_lora.sh
+++ deepspeed/run_llama2_7b_lora.sh
@@ -15,18 +15,21 @@ deepspeed main.py \
    --data_split 2,4,4 \
    --model_name_or_path meta-llama/Llama-2-7b-hf \
    --per_device_train_batch_size 1 \
-   --per_device_eval_batch_size 4 \
-   --max_seq_len 512 \
+   --per_device_eval_batch_size 1 \
+   --max_seq_len 256 \
    --learning_rate 9.65e-6 \
    --weight_decay 0. \
    --num_train_epochs 2  \
-   --gradient_accumulation_steps 4 \
+   --gradient_accumulation_steps 8 \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --seed 1234 \
    --gradient_checkpointing \
-   --dtype bf16 \
+   --dtype fp16 \
    --zero_stage $ZERO_STAGE \
    --deepspeed \
    --output_dir $OUTPUT \
-   #&> $OUTPUT/training.log
+   --lora_dim 8 \
+   --only_optimize_lora \
+   --lora_learning_rate 5e-4 \
+   &> $OUTPUT/training.log
